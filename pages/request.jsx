52b58399bb46e6283fc242ab027a0f33bb7df434
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-08-03 19:57:50 +0700

    Added safeguard for null currencies and rates from transfer and request page

--- pages/request.jsx
+++ pages/request.jsx
@@ -35,6 +35,27 @@ export default function Request({currencies, rates}) {
         
     })
 
+    if (!currencies || !rates) {
+        return (
+            <div>
+            <Head>
+                <title>DonDra</title>
+                <meta name="description" content="Solving your financial needs" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;500;800&family=Ramaraja&display=swap" rel="stylesheet"/> 
+            </Head>
+ 
+            <main className='flex w-full min-h-screen flex-col'>
+                <Navbar />
+                <h1 className='self-center font-bold text-white text-2xl mt-2'>Loading</h1>
+                
+            </main>
+        </div>
+        )
+    }
+
     const [saldo, setSaldo] = useState(0)
     const [error, setError] = useState('');
     const [nominal, setNominal] = useState(0);
